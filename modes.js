fe3631f600b46f894d6542959f0bd565a649826e
Alef Ramos <victorramos123@outlook.com>
2023-08-08 00:01:19 -0300

    ADD dark mode in the navbar

--- modes.js
+++ modes.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('.button-modes');
 const container = document.querySelector('.container');
+const navbar = document.querySelector('.navbar');
+const navlink = document.querySelector('.nav-link');
+const secondlink = document.querySelector('.navegator-link');
+const menuicon = document.querySelector('.navbar-toggler-icon');
 
 btn.onclick = function () {
-    this.classList.toggle('active')
-    container.classList.toggle('active');
+    this.classList.toggle('modechange')
+    container.classList.toggle('modechange');
+    navbar.classList.toggle('modechange');
+    navlink.classList.toggle('modechange');
+    secondlink.classList.toggle('modechange');
+    menuicon.classList.toggle('modechange');
 }
