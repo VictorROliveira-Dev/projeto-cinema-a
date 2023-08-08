fae5bf2cce7477efb6e5cf93db92892ae6c3f6ef
Alef Ramos <victorramos123@outlook.com>
2023-08-08 18:22:09 -0300

    add dark mode to the footer

--- modes.js
+++ modes.js
@@ -1,15 +1,26 @@
 const btn = document.querySelector('.button-modes');
 const container = document.querySelector('.container');
 const navbar = document.querySelector('.navbar');
-const navlink = document.querySelector('.nav-link');
-const secondlink = document.querySelector('.navegator-link');
 const menuicon = document.querySelector('.navbar-toggler-icon');
+let footer = document.querySelectorAll('footer');
+let footer_link = document.querySelectorAll('a');
+let footer_text = document.querySelectorAll('p');
 
 btn.onclick = function () {
     this.classList.toggle('modechange')
     container.classList.toggle('modechange');
     navbar.classList.toggle('modechange');
-    navlink.classList.toggle('modechange');
-    secondlink.classList.toggle('modechange');
     menuicon.classList.toggle('modechange');
+
+    footer.forEach(el => {
+        el.classList.toggle('modechange');
+    })
+
+    footer_text.forEach(el => {
+        el.classList.toggle('modechange');
+    })
+
+    footer_link.forEach(el => {
+        el.classList.toggle('modechange');
+    })
 }
