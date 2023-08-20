936551e8ca9b89e8d83c41039f8ed2a366743a88
Alef Ramos <victorramos123@outlook.com>
2023-08-20 13:14:00 -0300

    ADD dark mode to cards

--- modes.js
+++ modes.js
@@ -2,6 +2,9 @@ const btn = document.querySelector('.button-modes');
 const container = document.querySelector('.container');
 const navbar = document.querySelector('.navbar');
 const menuicon = document.querySelector('.navbar-toggler-icon');
+let cards = document.querySelectorAll('.card');
+let card_texts = document.querySelectorAll('h5');
+let button_cards = document.querySelectorAll('.button-cards');
 let footer = document.querySelectorAll('footer');
 let footer_link = document.querySelectorAll('a');
 let footer_text = document.querySelectorAll('p');
@@ -11,6 +14,18 @@ btn.onclick = function () {
     container.classList.toggle('modechange');
     navbar.classList.toggle('modechange');
     menuicon.classList.toggle('modechange');
+
+    cards.forEach(el => {
+        el.classList.toggle('modechange');
+    })
+
+    card_texts.forEach(el => {
+        el.classList.toggle('modechange');
+    })
+
+    button_cards.forEach(el => {
+        el.classList.toggle('modechange');
+    })
 
     footer.forEach(el => {
         el.classList.toggle('modechange');
